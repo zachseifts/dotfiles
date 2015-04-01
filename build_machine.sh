16cf83a1416553a6fc2137243c9863185fae2bf6
Zach Seifts <zach.seifts@gmail.com>
2015-04-01 13:03:57 -0400

    Installing homebrew cask with the other packages

--- build_machine.sh
+++ build_machine.sh
@@ -6,11 +6,8 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \ </dev/null
 brew doctor
 
-# Install homebrew packages
-brew install vim git ffmpeg tmux reattach-to-user-namespace ctags
-
-# Install homebrew-cask
-brew install caskroom/cask/brew-cask
+# Install homebrew packages and homebrew cask
+brew install vim git ffmpeg tmux reattach-to-user-namespace ctags caskroom/cask/brew-cask
 
 # Install homebrew-cask packages
 cask_packages=( adium alfred dropbox firefox google-chrome google-hangouts iterm2 spotify vagrant virtualbox vlc )

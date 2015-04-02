d0fc64ebbf7182ff257cb9d791abc8092c071da1
Zach Seifts <zach.seifts@gmail.com>
2015-04-02 10:02:01 -0400

    Adding google drive to the packages.

--- build_machine.sh
+++ build_machine.sh
@@ -10,7 +10,7 @@ brew doctor
 brew install vim git ffmpeg tmux reattach-to-user-namespace ctags caskroom/cask/brew-cask
 
 # Install homebrew-cask packages
-cask_packages=( adium alfred dropbox firefox google-chrome google-hangouts iterm2 spotify vagrant virtualbox vlc )
+cask_packages=( adium alfred dropbox firefox google-chrome google-drive google-hangouts iterm2 spotify vagrant virtualbox vlc )
 for app in "${cask_packages[@]}"
 do
   brew cask install --appdir=/Applications $app
